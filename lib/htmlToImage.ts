39910f4be108e8fb0a3f8bca80c07bd3ed4dce89
tkgka <ksh17171@gmail.com>
2021-11-25 18:29:14 +0900

    ./ no-sandbox 옵션 추가

--- lib/htmlToImage.ts
+++ lib/htmlToImage.ts
@@ -1,6 +1,6 @@
 import puppeteer from "puppeteer";
 export default async (html: any) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.goto(html);
     const imageBuffer = await page.screenshot({ omitBackground: true });
