9527c651d40292502869027b9a62b893f961a008
tkgka <ksh17171@gmail.com>
2021-11-27 23:45:37 +0900

    Update index.ts

--- routes/index.ts
+++ routes/index.ts
@@ -7,6 +7,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', async (req, res, next) => {
     const url = String(req.query.url)
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.send(await fullurl(String(url)))
 });
 
@@ -14,6 +16,8 @@ router.get('/img', async (req, res, next) => {
     var url = String(req.query.url)
     url.match(reg_pattern.pattern) ? url = url : url = `https://${url}`
     const imageBuffer = await htmlToImage(url);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.set("Content-Type", "image/png");
     res.send(imageBuffer);
 });
